4a42a9299ef56ea2cd0d48dae42818c959892414
BILL Tin <billtin@BILLs-MacBook-Pro.local>
2026-07-31 10:35:12 -0400

    fix: update route patterns in server.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 const app = require('../src/server');
 
 module.exports = (req, res) => {
-  if (req.url && req.url.startsWith('/api/index.js')) {
-    req.url = req.url.replace('/api/index.js', '') || '/';
+  if (req.url) {
+    if (req.url.startsWith('/api/index.js')) {
+      req.url = req.url.replace('/api/index.js', '') || '/';
+    } else if (req.url.startsWith('/api')) {
+      req.url = req.url.replace('/api', '') || '/';
+    }
   }
   return app(req, res);
 };
