5b6ad61141c03a13d7ab31604835b8948cd73b56
BILL Tin <billtin@BILLs-MacBook-Pro.local>
2026-08-01 20:44:33 -0400

    fix: remove conflicting serverless functions, use legacy routes to send all traffic to api/index.js Express app

--- api/manifest.js
+++
@@ -1,8 +0,0 @@
-const { manifest } = require('../src/addon');
-
-module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json(manifest);
-};
