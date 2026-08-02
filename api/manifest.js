0ad9aff282684f1cdeb94bbf1e863422ed66f1bc
BILL Tin <billtin@BILLs-MacBook-Pro.local>
2026-08-01 20:11:45 -0400

    fix: disable edge caching on manifest and top50 endpoints

--- api/manifest.js
+++ api/manifest.js
@@ -2,6 +2,7 @@ const { manifest } = require('../src/addon');
 
 module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json(manifest);
 };
--- api/top50.js
+++ api/top50.js
@@ -2,6 +2,7 @@ const top50Hub = require('../src/data_top50.json');
 
 module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json(top50Hub);
 };
