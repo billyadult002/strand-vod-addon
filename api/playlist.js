181b7746440f13bc7fc58c14b5b349c68a8434cd
BILL Tin <billtin@BILLs-MacBook-Pro.local>
2026-08-01 20:35:16 -0400

    fix: explicit .js extension in require('./playlist.m3u8.js')

--- api/playlist.js
+++ api/playlist.js
@@ -1,4 +1,4 @@
-const playlistM3u8Handler = require('./playlist.m3u8');
+const playlistM3u8Handler = require('./playlist.m3u8.js');
 
 module.exports = (req, res) => {
   return playlistM3u8Handler(req, res);
