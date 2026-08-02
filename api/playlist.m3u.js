5f88a91cc23ed78086df4972558ed600ddbf69fd
BILL Tin <billtin@BILLs-MacBook-Pro.local>
2026-08-01 20:23:44 -0400

    refactor: use clean single Express catch-all in api/index.js for all endpoints

--- api/playlist.m3u.js
+++
@@ -1,5 +0,0 @@
-const playlistM3u8 = require('./playlist.m3u8');
-
-module.exports = (req, res) => {
-  return playlistM3u8(req, res);
-};
--- api/sources.js
+++
@@ -1,7 +0,0 @@
-const { vodSources } = require('../src/maccms');
-
-module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json(vodSources);
-};
